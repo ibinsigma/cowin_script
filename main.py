d5df01f1f4eab9142e06ac841b68e67647adf705
Niloy <niloy.sarkar@learner.manipal.edu>
2021-05-03 06:43:47 +0530

    Available centers functionality

--- main.py
+++ main.py
@@ -1,19 +1,43 @@
+import datetime
+import logging
 import requests
-from flask import Flask, request
+from flask import Flask, request, render_template, redirect, url_for, session
+from datetime import date
 
 app = Flask(__name__)
+app.secret_key = "super secret key"
+
+log = logging.getLogger('cowin_script')
+logging.basicConfig(level=logging.INFO)
 
 
 @app.route('/')
 def hello():
-    return "Hello World!"
+    return render_template('index.html')
 
 
-@app.route('/cowin')
+@app.route("/", methods=['POST'])
+def get_values():
+    email = request.form['inputEmail']
+    phone = request.form['inputPhone']
+    age = request.form['inputAge']
+    district = request.form['inputDistrict']
+    data = {'email': email, 'phone': phone, 'age': age, 'district': district}
+    session['data'] = data
+    log.info(session['data'])
+    return redirect(url_for('cowin'))
+
+
+@app.route('/cowin', methods=['GET', 'POST'])
 def cowin():
-    district_id = request.args['district_id']
-    date = request.args['date']
-    response_json_dict = get_json(district_id, date)
+    # district_id = request.args['district_id']
+    # date = request.args['date']
+    today = datetime.date.today()
+    date_today = today.strftime("%d-%m-%Y")
+    log.info('  DATE  ' + str(date_today))
+    data = session['data']
+    log.info('  SESSION DATA  ' + str(data))
+    response_json_dict = get_json(data['district'], date_today)
     final_dict_open = parse_response_json(response_json_dict)
     return final_dict_open
 
@@ -22,42 +46,32 @@ def get_json(district_id, date):
     response = requests.get('https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict'
                             '?district_id={district_id}&date={date}'.format(district_id=district_id, date=date))
     response_json_dict = response.json()
-    # print(response_json)
+    log.info('  STATUS_CODE  ' + str(response.status_code))
     return response_json_dict
 
 
 def parse_response_json(response_json):
     list_centers = response_json['centers']
     total_centers = len(list_centers)
-    # print(total_centers)
+    log.info('  TOTAL CENTERS IN DISTRICT  ' + str(total_centers))
     open_centers = {}
+    data = session['data']
     for center in list_centers:
         center_name = center['name']
         center_sessions = center['sessions']
         open_sessions = []
-        open_18_sessions = []
-        for session in center_sessions:
-            if session['available_capacity'] > 0:
-                # print(center_name)
-                # print(session['date'])
-                # print(session['available_capacity'])
-                # print(session['min_age_limit'])
-                open_sessions.append(session)
-                # print(session)
-                # print(type(session))
-            if session['min_age_limit'] < 45 and session['available_capacity'] > 0:
-                open_18_sessions.append(session)
-                print(center_name)
-                print('FINALLY!')
-        open_centers[center_name] = open_sessions
+        flag = False
+        for session_of_center in center_sessions:
+            if session_of_center['available_capacity'] > 0 and int(session_of_center['min_age_limit']) == int(data['age']):
+                flag = True
+                open_sessions.append(session_of_center)
+        if flag:
+            open_centers[center_name] = open_sessions
+    if len(open_centers) == 0:
+        return "Sorry! No centers available right now"
     return open_centers
 
 
-# if __name__ == '__main__':
-#
-#     district_id = 240
-#     date = '02-05-2021'
-#     # for i in range(1, 737):
-#     # district_id = i
-#     response_json = get_json(district_id, date)
-#     parse_response_json(response_json)
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
