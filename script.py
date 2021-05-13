4d2aa3552ec4c3fe85489d45b25c7102b5fdd41c
Niloy <niloy.sarkar@learner.manipal.edu>
2021-05-13 14:55:39 +0530

    Ranchi slots

--- script.py
+++ script.py
@@ -4,33 +4,40 @@ import requests
 import time
 from playsound import playsound
 
-
 def get_json(district_id, date):
-    response = requests.get('https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict'
-                            '?district_id={district_id}&date={date}'.format(district_id=district_id, date=date))
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+    response = requests.get('https://cdn-api.co-vin.in/api/v2/appointment/sessions/calendarByDistrict'
+                            '?district_id={district_id}&date={date}'.format(district_id=district_id, date=date), headers=headers)
+    print(response.status_code)
+    while response.status_code == 401:
+        print('Retry')
+        response = requests.get('https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict'
+                                '?district_id={district_id}&date={date}'.format(district_id=district_id, date=date),
+                                headers=headers)
     response_json_dict = response.json()
     parse_response_json(response_json_dict)
 
 
 def parse_response_json(response_json):
     list_centers = response_json['centers']
-    # print(len(list_centers))
+    print(len(list_centers))
     for center in list_centers:
         center_name = center['name']
         center_sessions = center['sessions']
         for session in center_sessions:
-            if session['min_age_limit'] < 45 and session['available_capacity'] > 0:
+            if session['min_age_limit'] < 45 and session['available_capacity'] > 3:
                 print(center_name)
                 print('FINALLY!')
                 playsound('notif.mp3')
+                # quit()
 
 
 if __name__ == '__main__':
-    district_id = 725
+    district_id = 240
     today = datetime.date.today()
     date = today.strftime("%d-%m-%Y")
     schedule.every(10).seconds.do(get_json, district_id, date)
-    playsound('notif.mp3')
+    # playsound('notif.mp3')
     while True:
         schedule.run_pending()
         time.sleep(1)
